c1279a29a1206d93412ac8ce39b93a8356289091
DesignQu <250533855@qq.com>
2017-02-17 10:02:05 +0800

    无

--- App/src/main/java/com/custom/view/activity/MainActivity.java
+++ App/src/main/java/com/custom/view/activity/MainActivity.java
@@ -11,6 +11,7 @@ import com.custom.view.activity.base.BaseActivity;
 import com.custom.view.activity.view.AutoHorizontalScrollTextViewActivity;
 import com.custom.view.activity.view.AutoVerticalScrollTextViewActivity;
 import com.custom.view.activity.view.BadgeViewActivity;
+import com.custom.view.activity.view.BlurringViewActivity;
 import com.custom.view.activity.view.CircleImageViewActivity;
 import com.custom.view.activity.view.CountTimeProgressViewActivity;
 import com.custom.view.activity.view.FlowRadioGroupActivity;
@@ -37,7 +38,8 @@ public class MainActivity extends BaseActivity {
             new CustomView(R.string.view_title_waveView, R.string.view_introduction_waveView, WaveViewActivity.class),
             new CustomView(R.string.view_title_badgeView, R.string.view_introduction_badgeView, BadgeViewActivity.class),
             new CustomView(R.string.view_title_circleImageView, R.string.view_introduction_circleImageView, CircleImageViewActivity.class),
-            new CustomView(R.string.view_title_countTimeProgressView, R.string.view_introduction_countTimeProgressView, CountTimeProgressViewActivity.class)
+            new CustomView(R.string.view_title_countTimeProgressView, R.string.view_introduction_countTimeProgressView, CountTimeProgressViewActivity.class),
+            new CustomView(R.string.view_title_blurringView, R.string.view_introduction_blurringView, BlurringViewActivity.class)
     };
 
     @Override
---
+++ App/src/main/java/com/custom/view/activity/view/BlurringViewActivity.java
@@ -0,0 +1,131 @@
+package com.custom.view.activity.view;
+
+import android.animation.Animator;
+import android.animation.AnimatorSet;
+import android.animation.ObjectAnimator;
+import android.animation.ValueAnimator;
+import android.os.Bundle;
+import android.view.View;
+import android.view.animation.OvershootInterpolator;
+import android.widget.ImageView;
+
+import com.custom.view.R;
+import com.custom.view.activity.base.BaseActivity;
+import com.custom.view.view.AutoHorizontalScrollTextView;
+import com.custom.view.view.BlurringView;
+
+import java.util.Random;
+
+/**
+ * @author https://github.com/500px/500px-android-blur
+ */
+public class BlurringViewActivity extends BaseActivity {
+
+    private BlurringView mBlurringView;
+
+    private static final int[] IMAGE_IDS = {
+            R.drawable.puzzle, R.drawable.puzzle, R.drawable.puzzle, R.drawable.puzzle, R.drawable.puzzle,
+            R.drawable.puzzle, R.drawable.puzzle, R.drawable.puzzle, R.drawable.puzzle, R.drawable.puzzle
+    };
+
+    private ImageView[] mImageViews = new ImageView[9];
+    private int mStartIndex;
+
+    private Random mRandom = new Random();
+
+    private boolean mShifted;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_blurringview);
+
+        setTitle(BlurringView.class.getSimpleName());
+
+        this.init();
+    }
+
+    private void init() {
+        mBlurringView = (BlurringView) findViewById(R.id.blurring_view);
+        View blurredView = findViewById(R.id.blurred_view);
+
+        // Give the blurring view a reference to the blurred view.
+        mBlurringView.setBlurredView(blurredView);
+
+        mImageViews[0] = (ImageView) findViewById(R.id.image0);
+        mImageViews[1] = (ImageView) findViewById(R.id.image1);
+        mImageViews[2] = (ImageView) findViewById(R.id.image2);
+        mImageViews[3] = (ImageView) findViewById(R.id.image3);
+        mImageViews[4] = (ImageView) findViewById(R.id.image4);
+        mImageViews[5] = (ImageView) findViewById(R.id.image5);
+        mImageViews[6] = (ImageView) findViewById(R.id.image6);
+        mImageViews[7] = (ImageView) findViewById(R.id.image7);
+        mImageViews[8] = (ImageView) findViewById(R.id.image8);
+    }
+
+    private ValueAnimator.AnimatorUpdateListener listener = new ValueAnimator.AnimatorUpdateListener() {
+        @Override
+        public void onAnimationUpdate(ValueAnimator animation) {
+            mBlurringView.invalidate();
+        }
+    };
+
+    public void shift(View view) {
+        if (!mShifted) {
+            for (ImageView imageView : mImageViews) {
+                ObjectAnimator tx = ObjectAnimator.ofFloat(imageView, View.TRANSLATION_X, (mRandom.nextFloat() - 0.5f) * 500);
+                tx.addUpdateListener(listener);
+                ObjectAnimator ty = ObjectAnimator.ofFloat(imageView, View.TRANSLATION_Y, (mRandom.nextFloat() - 0.5f) * 500);
+                ty.addUpdateListener(listener);
+                AnimatorSet set = new AnimatorSet();
+                set.playTogether(tx, ty);
+                set.setDuration(3000);
+                set.setInterpolator(new OvershootInterpolator());
+                set.addListener(new AnimationEndListener(imageView));
+                set.start();
+            }
+            mShifted = true;
+        } else {
+            for (ImageView imageView : mImageViews) {
+                ObjectAnimator tx = ObjectAnimator.ofFloat(imageView, View.TRANSLATION_X, 0);
+                tx.addUpdateListener(listener);
+                ObjectAnimator ty = ObjectAnimator.ofFloat(imageView, View.TRANSLATION_Y, 0);
+                ty.addUpdateListener(listener);
+                AnimatorSet set = new AnimatorSet();
+                set.playTogether(tx, ty);
+                set.setDuration(3000);
+                set.setInterpolator(new OvershootInterpolator());
+                set.addListener(new AnimationEndListener(imageView));
+                set.start();
+            }
+            mShifted = false;
+        }
+    }
+
+    private static class AnimationEndListener implements Animator.AnimatorListener {
+
+        View mView;
+
+        public AnimationEndListener(View v) {
+            mView = v;
+        }
+
+        @Override
+        public void onAnimationStart(Animator animation) {
+            mView.setLayerType(View.LAYER_TYPE_HARDWARE, null);
+        }
+
+        @Override
+        public void onAnimationEnd(Animator animation) {
+            mView.setLayerType(View.LAYER_TYPE_NONE, null);
+        }
+
+        @Override
+        public void onAnimationCancel(Animator animation) {
+            mView.setLayerType(View.LAYER_TYPE_NONE, null);
+        }
+
+        @Override
+        public void onAnimationRepeat(Animator animation) {}
+    }
+}
